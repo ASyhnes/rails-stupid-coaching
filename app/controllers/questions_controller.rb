67fb69da66baaaed288e344beb73aeb6752b79d2
david chardon <syhnes@gmail.com>
2024-08-05 14:22:43 +0200

    stupid coatching on rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
 class QuestionsController < ApplicationController
 
   def ask
+    # est appellée pars view/ask
   end
 
   def answer
+    # est appellée pars view/answer
     @ask = params[:test ]
+    # correspond a la clef du hash produit par la méthode ask
     @answer =
     if @ask == "I am going to work"
       "Great!"
